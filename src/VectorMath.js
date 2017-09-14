8bb80b4f0b7531bfe6ebbdd42b3835e8de79b663
Jonathan Holmlund <jonoh89@gmail.com>
2017-09-14 19:55:40 +0200

    add normalise and dot math

--- src/VectorMath.js
+++ src/VectorMath.js
@@ -1,3 +1,5 @@
+import * as PIXI from 'pixi.js';
+
 export const add = (point1, point2) => [
   point1.x + point2.x,
   point1.y + point2.y
@@ -20,3 +22,17 @@ export const distance = (point1, point2) => {
   const [x, y] = subtract(point1, point2);
   return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 };
+
+export const divide = (point1, point2) => ([point1.x / point2.x, point1.y / point2.y]);
+
+export const normalize = (point) => {
+  const length = length(point);
+
+  if (length === 0) {
+    return [1, 0];
+  }
+
+  return divide(point, new PIXI.Point(length, length));
+};
+
+export const dot = (point1, point2) => point1.x * point2.x + point1.y * point2.y;
